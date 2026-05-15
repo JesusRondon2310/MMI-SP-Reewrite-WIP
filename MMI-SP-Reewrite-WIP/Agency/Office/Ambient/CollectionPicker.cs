f40ac18ea750b816a7c9a56b2f38362dac1a18f2
JesusRondon2310 <126926654+JesusRondon2310@users.noreply.github.com>
2026-05-15 19:14:16 +0200

    Add files via upload

---
+++ MMI-SP-Reewrite-WIP/Agency/Office/Ambient/CollectionPicker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using GTA;
+using GTA.Chrono;
+using static MMI_SP.Agency.ItemsManager;
+
+namespace MMI_SP.Agency.Office.Ambient
+{
+    internal static class CollectionPicker
+    {
+        // ==========================================
+        // BLOQUE 1: Funciones
+        // ==========================================
+        public static OfficeItemsCollection Pick()
+        {
+            Random rnd = new Random(Game.GameTime);
+            List<OfficeItemsCollection> pool = new List<OfficeItemsCollection>();
+
+            int hour = GameClock.Hour;
+
+            if (hour >= 2 && hour < 12)
+            {
+                pool.AddRange(ItemsManager.GetItemsCollection(CollectionType.Normal));
+            }
+            else if (hour >= 12 && hour < 14)
+            {
+                pool.AddRange(ItemsManager.GetItemsCollection(CollectionType.Midday));
+                pool.AddRange(ItemsManager.GetItemsCollection(CollectionType.Normal));
+            }
+            else if (hour >= 14)
+            {
+                pool.AddRange(ItemsManager.GetItemsCollection(CollectionType.Normal));
+            }
+            else
+            {
+                pool.AddRange(ItemsManager.GetItemsCollection(CollectionType.Normal));
+                pool.AddRange(ItemsManager.GetItemsCollection(CollectionType.Night));
+            }
+
+            return pool[rnd.Next(0, pool.Count)];
+        }
+    }
+}
---
+++ MMI-SP-Reewrite-WIP/Agency/Office/Ambient/Handler.cs
@@ -0,0 +1,32 @@
+﻿using static MMI_SP.Agency.ItemsManager;
+
+namespace MMI_SP.Agency.Office.Ambient
+{
+    internal class Handler
+    {
+        // ==========================================
+        // BLOQUE 1: Variables de la clase y creación del objeto
+        // ==========================================
+        internal OfficeItemsCollection ItemsCollection { get; private set; }
+
+        public Handler()
+        {
+            ItemsCollection = new OfficeItemsCollection(CollectionPicker.Pick());
+            Builder.Build(ItemsCollection);
+        }
+
+        public Handler(OfficeItemsCollection collection)
+        {
+            ItemsCollection = new OfficeItemsCollection(collection);
+            Builder.Build(ItemsCollection);
+        }
+
+        // ==========================================
+        // BLOQUE 2: Funciones
+        // ==========================================
+        internal void CleanOffice()
+        {
+            CleanUp.Execute();
+        }
+    }
+}
